d10f639ca79a451607df39bc9c0cb9221f4a072e
David Knight <DKnight96@sbcglobal.net>
2025-09-22 13:36:53 -0700

    Create livetrack MVP

---
+++ livetrack.py
@@ -0,0 +1,99 @@
+import time
+import socket
+from datetime import datetime
+
+import numpy as np
+import pymap3d
+
+from PlutoLogger import PlutoLogger
+
+# Create a socket object
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to a server (replace with your server's address and port)
+server_address = ('localhost', 30003)
+sock.connect(server_address)
+
+socket_file = sock.makefile('rb')
+
+# transmitterLLA = np.array([34.052724, -117.596634, 282])
+transmitterLLA = np.array([34.1334345,-117.9070175, 198])
+
+transmitterEl = [40, 50] # Testing values for now
+
+IDX_ICAO = 4
+IDX_CALLSIGN = 5
+IDX_ALT = 11
+IDX_LAT = 14
+IDX_LON = 15
+
+trackedAircraft = {} # icao : tLast
+aicraftTimeout = 15
+
+pluto = PlutoLogger()
+pluto.start()
+
+tLastSleep = datetime.now()
+
+try:
+    while True:
+        # Save some cpu
+        if (datetime.now() - tLastSleep).total_seconds() > 0.25:
+            time.sleep(0.25)
+            pass
+
+        # Prune old entries
+        aicraftToRemove = []
+        for icao in trackedAircraft:
+            if (datetime.now() - trackedAircraft[icao]).total_seconds() > aicraftTimeout:
+                aicraftToRemove.append(icao)
+
+        for icao in aicraftToRemove:
+            print(f"{icao} exited")
+            pluto.stopRecording(icao)
+            del trackedAircraft[icao]
+
+        # Try parsing next message
+        line = socket_file.readline()
+        if not line:  # Empty line indicates end of stream (connection closed)
+            break
+        line = line.decode('utf-8')
+        if "MSG,3" not in line:
+            continue
+        line = line.split(',')
+        if line[IDX_LAT] == '':
+            continue
+        if line[IDX_LON] == '':
+            continue
+        if line[IDX_ALT] == '':
+            continue
+        icao = line[IDX_ICAO]
+        lat = float(line[IDX_LAT])
+        lon = float(line[IDX_LON])
+        alt = float(line[IDX_ALT])
+
+        if np.isclose(lat, 0.0):
+            continue
+        if np.isclose(lon, 0.0):
+            continue
+        if np.isclose(alt, 0.0):
+            continue
+        now = datetime.now()
+
+        aircraftLLA = np.array([lat, lon, alt])
+
+        aircraftAER = pymap3d.geodetic2aer(*aircraftLLA, *transmitterLLA)
+        if aircraftAER[1] > transmitterEl[0] and aircraftAER[1] < transmitterEl[1]:
+            if icao not in trackedAircraft:
+                fname = now.isoformat(timespec='seconds') + "_" + icao + '.dat'
+                pluto.startRecording(icao, fname)
+                print(f"{icao} entered")
+            
+            trackedAircraft[icao] = now
+
+finally:
+    pluto.stop()
+
+    # Close the file-like object and the socket
+    socket_file.close()
+    sock.close()
