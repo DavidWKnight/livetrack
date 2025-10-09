e63be4c070050d00fedfde90d5851eaa0653ac92
David Knight <DKnight96@sbcglobal.net>
2025-10-08 20:13:21 -0700

    moved some process code into rfTypes.py

--- process.py
+++ process.py
@@ -1,103 +1,30 @@
-import copy
-import time
-import glob
-import csv
-from datetime import datetime, timedelta
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import ndimage
-from scipy import signal
-import pymap3d
 
 import dataLoad
-from radarAlgo import maxPool1d, findDirectPathPulses, pulseIntegration, matchFilter
-from ACState import ACPosition
+from rfTypes import collect2scans
 
 def lin2db(x):
     return 20 * np.log10(x)
 
-folder = '/home/david/Documents/Grad_School/Bistatic-Radar/Collects/collect_9_28/collects4/'
-fnameBase = '2025-09-28T17:10:12_A0851B'
+folder = '/media/david/Ext/Collects/collects_10_6/collects2/'
+# folder = '/media/david/Ext/Collects/collect_9_28/collects5/'
+# fnameBase = '2025-10-04T12:08:50_ABC504'
 # fnameBase = '2025-09-28T17:06:03_A3AA32'
-# fnameBase = '2025-09-28T17:02:31_A3FBA4'
-transmitterLLA = np.array([34.052724, -117.596634, 0])
-receiverLLA = [34.051555, -117.593415, 0]
-recieverAER = pymap3d.geodetic2aer(*receiverLLA, *transmitterLLA)
+fnameBase = '2025-10-06T17:26:46_AC98AC'
+# fnameBase = '2025-09-28T17:17:35_A420AF'
 
-aircraftState, data = dataLoad.loadCollect(folder + fnameBase)
-data = data[4:] # Remove header
-data = data[::2] + -1j * data[1::2]
+aircraftState, settings, data = dataLoad.loadCollect(folder + fnameBase)
 data = np.absolute(data)
 
-# data = np.absolute(data[:int(len(data) / 4)])
-# data = data - np.mean(data)
-# fftData = np.fft.fft(data)
-# magResponse = np.absolute(fftData)
-# magResponse = lin2db(magResponse)
-# plt.plot(magResponse[:int(len(magResponse) / 2)])
-# plt.show()
+scans = collect2scans(data, settings)
 
-tStart = aircraftState.positions[0].t
+for scan in scans:
+    scan.appendTarget(aircraftState)
 
-sampleRate = 1e6 * 0.75
-
-start = time.time()
-# data = matchFilter(data, sampleRate)
-data = pulseIntegration(data, sampleRate)
-end = time.time()
-print(f"pulseIntegration took {end - start} seconds")
-
-directPath, directPathIdx = findDirectPathPulses(data, sampleRate, 1, False)
-
-aircraftDetTime = []
-aircraftDetTimeGuess = []
-for frameStart in directPath:
-    tDet = 0
-    nIterations = 2
-    for i in range(nIterations):
-        tProp = tStart + timedelta(seconds=(frameStart+tDet))
-        aircraftLLAFrameStart = aircraftState.getPosition(tProp)
-        
-        extrap = False
-        if aircraftLLAFrameStart is None: # Requested time is outside recorded positions
-            extrap = True
-            aircraftLLAFrameStart = aircraftState.getPosition(tProp, True)
-        
-        aircraftAERFrameStart = pymap3d.geodetic2aer(*aircraftLLAFrameStart, *transmitterLLA)
-        dAz = aircraftAERFrameStart[0] - recieverAER[0]
-        if dAz < 0:
-            dAz = dAz + 360
-        tDet = (dAz/360) * 4.6
-
-    if extrap:
-        aircraftDetTimeGuess.append(frameStart + tDet)
-    else:
-        aircraftDetTime.append(frameStart + tDet)
-
-decimate = 10
-data = maxPool1d(data, decimate)
-sampleRate = sampleRate / decimate
-
-# data = data[int((sampleRate/decimate)*3):int((sampleRate/decimate)*5)]
-data = lin2db(data)
-
-asrThreshold = np.percentile(data, 95)
-
-print(directPath)
-print(np.diff(directPath))
-retAmplitude = data[np.array(directPathIdx) // decimate]
-print(retAmplitude)
-print(aircraftDetTime)
-
-plt.plot(np.array(range(0, len(data))) * (1/sampleRate), data)
-plt.axhline(y=asrThreshold, color='black', linestyle='--')
-plt.scatter(directPath, retAmplitude, color='gray')
-
-# Put lines where we expect aircaft
-for t in aircraftDetTime:
-    plt.axvline(t, color='r', linestyle='--')
-for t in aircraftDetTimeGuess:
-    plt.axvline(t, color='peru', linestyle='--')
-
-plt.show()
+for scan in scans:
+    plt.plot(scan.getDataTimes() - scan.tStart, lin2db(scan.data))
+    for tDet in scan.targetTimes:
+        plt.axvline(tDet - scan.tStart,  color='r', linestyle='--')
+    plt.show()
---
+++ rfTypes.py
@@ -0,0 +1,89 @@
+from typing import List
+from datetime import datetime, timedelta
+
+import numpy as np
+import pymap3d
+
+from ACState import ACState
+from radarAlgo import findDirectPathPulses
+
+class Return():
+    AER: np.ndarray
+    t: datetime
+
+    def __init__(self):
+        pass
+
+
+class Frame():
+    def __init__(self):
+        pass
+
+    def getReturns(self):
+        # Perform CFAR, take CFAR parameters as args
+        pass
+
+class Scan():
+    data: np.ndarray
+    settings: int
+    tStart: float
+    targetTimes = List[float]
+
+    def __init__(self, data, settings, tStart):
+        self.data = data
+        self.settings = settings
+        self.tStart = tStart
+        self.targetTimes = []
+
+    def appendTarget(self, target: ACState) -> bool:
+        transmitterLLA = self.settings['transmitterLLA']
+        receiverLLA = self.settings['receiverLLA']
+        recieverAER = pymap3d.geodetic2aer(*receiverLLA, *transmitterLLA)
+        
+        tDet = 0 # Initial guess
+        nIterations = 2
+
+        collectStart = self.settings['tStart']
+
+        for _ in range(nIterations):
+            tProp = collectStart + timedelta(seconds= self.tStart + tDet)
+            targetLLA = target.getPosition(tProp)
+            if targetLLA is None:
+                return False
+            aircraftAER = pymap3d.geodetic2aer(*targetLLA, *transmitterLLA)
+            dAz = aircraftAER[0] - recieverAER[0]
+            if dAz < 0:
+                dAz = dAz + 360
+            # Could early return if dAz is small
+            tDet = (dAz/360) * 4.6
+        
+        self.targetTimes.append(self.tStart + tDet)
+        return True
+
+    def getDataTimes(self) -> np.ndarray:
+        """Return the time that each scan data point is at."""
+        sampleRate = self.settings['sampleRate']
+        idxs = np.array(range(0, len(self.data)))
+        tValues = self.tStart + (idxs/sampleRate)
+        return tValues
+
+
+def collect2scans(data: np.ndarray, settings) -> List[Scan]:
+    sampleRate = settings['sampleRate']
+    try:
+        directPath, directPathIdx = findDirectPathPulses(data, sampleRate, 1, False)
+    except:
+        # Can't find pulses, just plot with no offset
+        print(f"Unable to find direct pulses, aligning to start of collect")
+        tCollect = len(data) / sampleRate
+        nFullScans = int(np.floor(tCollect / 4.6))
+        directPath = np.array(range(nFullScans))*4.6
+        directPathIdx = directPath * sampleRate
+
+    scans = []
+    for idxStart, idxEnd in zip(directPathIdx[:-1], directPathIdx[1:]):
+        scanData = data[idxStart:idxEnd]
+        tStart = idxStart / sampleRate
+        scans.append(Scan(scanData, settings, tStart))
+
+    return scans
